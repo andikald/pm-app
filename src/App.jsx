083ddd9a70909e778945f55b4ef1f5e0eedfbee7
Andika Aldi <andikald16@gmail.com>
2026-04-13 11:21:05 +0700

    force update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import "./App.css";
-
 export default function App() {
-  return (
-    <div>
-      <h1>PM App 🚀</h1>
-      <p>Welcome Aldi</p>
-    </div>
-  );
+  return <h1>PM App 🚀</h1>;
+}export default function App() {
+  return <h1>PM App 🚀</h1>;
 }
